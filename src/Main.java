799e6294e7e087b1a1d8fe4fbd0d8eef7bf27f86
Anupa200212 <supulanupa@gmail.com>
2026-01-18 18:28:13 +0530

    com 1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Vehicle v1 = new Vehicle();
+        Bus bus = new Bus();
+        Car car =new Car();
+
+        bus.color="White";
+        bus.year=2015;
+
+        bus.drive();
+        bus.start();
+        System.out.println( bus.color);
+        System.out.println( bus.year);
+    }
+}
