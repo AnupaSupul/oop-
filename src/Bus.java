dab975eeefaacd7403282fe08bc738e4ba6a0521
Anupa200212 <supulanupa@gmail.com>
2026-01-18 18:44:23 +0530

    com 3

---
+++ src/Bus.java
@@ -0,0 +1,5 @@
+public class Bus extends Vehicle{
+    public  void  dooropen(){
+        System.out.println("dooropen in bus");
+    }
+}
---
+++ src/Car.java
@@ -0,0 +1,6 @@
+public class Car extends Vehicle {
+
+    public  String glassopen(){
+        return "dooropen in car";
+    }
+}
